534032ae4b11e5e735ebdd416520b5404b2e96f5
tomaszek2115 <tomaush02@gmail.com>
2026-06-20 18:07:22 +0200

    Initial Commit

---
+++ eplate/eplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  eplateApp.swift
+//  eplate
+//
+//  Created by tomaszku on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct eplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
